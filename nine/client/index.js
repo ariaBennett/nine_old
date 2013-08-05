e2bf853d5647ec1f99e02f2534a3e99ef44ff35d
AriaB <ariasayshi@gmail.com>
2013-08-05 10:52:30 -0700

    Cool stuff lets make fft now.

--- nine/client/index.js
+++ nine/client/index.js
@@ -46,86 +46,86 @@ nine = (function() {
 
 
 Meteor.startup(function() {
-// set the scene size
-var WIDTH = 400,
-  HEIGHT = 300;
+  // set the scene size
+  var WIDTH = 400,
+    HEIGHT = 300;
 
-// set some camera attributes
-var VIEW_ANGLE = 45,
-  ASPECT = WIDTH / HEIGHT,
-  NEAR = 0.1,
-  FAR = 10000;
+  // set some camera attributes
+  var VIEW_ANGLE = 45,
+    ASPECT = WIDTH / HEIGHT,
+    NEAR = 0.1,
+    FAR = 10000;
 
-// get the DOM element to attach to
-// - assume we've got jQuery to hand
-var $container = $('#gameviewContainer');
+  // get the DOM element to attach to
+  // - assume we've got jQuery to hand
+  var $container = $('#gameviewContainer');
 
-// create a WebGL renderer, camera
-// and a scene
-var renderer = new THREE.WebGLRenderer();
-var camera =
-  new THREE.PerspectiveCamera(
-    VIEW_ANGLE,
-    ASPECT,
-    NEAR,
-    FAR);
+  // create a WebGL renderer, camera
+  // and a scene
+  var renderer = new THREE.WebGLRenderer();
+  var camera =
+    new THREE.PerspectiveCamera(
+      VIEW_ANGLE,
+      ASPECT,
+      NEAR,
+      FAR);
 
-var scene = new THREE.Scene();
+  var scene = new THREE.Scene();
 
-// add the camera to the scene
-scene.add(camera);
+  // add the camera to the scene
+  scene.add(camera);
 
-// the camera starts at 0,0,0
-// so pull it back
-camera.position.z = 300;
+  // the camera starts at 0,0,0
+  // so pull it back
+  camera.position.z = 400;
 
-// start the renderer
-renderer.setSize(WIDTH, HEIGHT);
+  // start the renderer
+  renderer.setSize(WIDTH, HEIGHT);
 
-// attach the render-supplied DOM element
-$container.append(renderer.domElement);
+  // attach the render-supplied DOM element
+  $container.append(renderer.domElement);
 
-// set up the sphere vars
-var radius = 50,
-    segments = 16,
-    rings = 16;
+  // set up the sphere vars
+  var radius = 50,
+      segments = 16,
+      rings = 16;
 
-// create the sphere's material
-var sphereMaterial =
-  new THREE.MeshLambertMaterial(
-    {
-      color: 0xCC0000
-    });
+  // create the sphere's material
+  var sphereMaterial =
+    new THREE.MeshLambertMaterial(
+      {
+        color: 0xCC0000
+      });
 
-// create a new mesh with
-// sphere geometry - we will cover
-// the sphereMaterial next!
-var sphere = new THREE.Mesh(
+  // create a new mesh with
+  // sphere geometry - we will cover
+  // the sphereMaterial next!
+  var sphere = new THREE.Mesh(
 
-  new THREE.SphereGeometry(
-    radius,
-    segments,
-    rings),
+    new THREE.SphereGeometry(
+      radius,
+      segments,
+      rings),
 
-  sphereMaterial);
+    sphereMaterial);
 
-// add the sphere to the scene
-scene.add(sphere);
+  // add the sphere to the scene
+  scene.add(sphere);
 
 
-// create a point light
-var pointLight =
-  new THREE.PointLight(0xFFFFFF);
+  // create a point light
+  var pointLight =
+    new THREE.PointLight(0xFFFFFF);
 
-// set its position
-pointLight.position.x = 10;
-pointLight.position.y = 50;
-pointLight.position.z = 130;
+  // set its position
+  pointLight.position.x = 10;
+  pointLight.position.y = 50;
+  pointLight.position.z = 130;
 
-// add to the scene
-scene.add(pointLight);
+  // add to the scene
+  scene.add(pointLight);
 
-// draw!
-renderer.render(scene, 
-   camera);
+  // draw!
+  renderer.render(scene, 
+     camera);
 });
